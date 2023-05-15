fc11ab479ef1125db69819ec58bad68ca74bdca1
Vlad Gorbachev <vlad-gorbachev-96@mail.ru>
2023-05-15 22:03:03 +0900

    Version3

--- Music BD.sql
+++ Music BD.sql
@@ -1,5 +1,5 @@
 create table if not exists Genre_of_music (
-Genre SERIAL primary key,
+GenreID SERIAL primary key,
 Name VARCHAR (20) unique not null
 );
 
@@ -9,35 +9,38 @@ Name VARCHAR (20) unique not null
 );
 
 create table if not exists GroupGenre (
-id serial primary key,
-MusicalGroupID integer references Musical_group(GroupID),
-GenreOfMusicID integer references Genre_of_music(Genre)
+ID SERIAL primary key,
+MusicalGroupID INTEGER references Musical_group(GroupID),
+GenreOfMusicID INTEGER references Genre_of_music(GenreID)
 );
 
 create table if not exists Albom (
-AlbomTitle VARCHAR (30) not null primary key,
-YearOfAlbom integer not null
+IDAlbom SERIAL primary key,
+AlbomTitle VARCHAR (30) not null,
+YearOfAlbom INTEGER not null check (YearOfAlbom < 2024)
 );
 
 create table if not exists GroupAlbom (
-id serial primary key,
-albom VARCHAR (30) references Albom(AlbomTitle),
-groupID integer references Musical_group(GroupID)
+id SERIAL primary key,
+albomID INTEGER references Albom(IDAlbom),
+groupID INTEGER references Musical_group(GroupID)
 );
 
 create table if not exists Music (
-Title VARCHAR (50) not null primary key,
-Albom VARCHAR (30) not null references Albom(AlbomTitle),
-Duration NUMERIC not null
+IDMusic SERIAL primary key,
+Title VARCHAR (50) not null,
+Albom VARCHAR (30) not null references Albom(IDAlbom),
+Duration INTEGER not null check (Duration < 601)
 );
 
 create table if not exists Compilation (
-NameCompilation VARCHAR (50) not null unique primary key,
-yearCompilation integer not null
+IDCompilation SERIAL primary key,
+NameCompilation VARCHAR (50) not null unique,
+yearCompilation INTEGER not null check (yearCompilation < 2024)
 );
 
 create table if not exists CompilationMusic (
 ID SERIAL primary key,
-titleMusic VARCHAR (50) references Music(Title),
-compilation VARCHAR (50) references Compilation(NameCompilation)
+titleMusic INTEGER references Music(IDMusic),
+compilation INTEGER references Compilation(IDCompilation)
 );
